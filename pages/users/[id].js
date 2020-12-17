52cc0a71a8085a4aa0cc4bbc309837b9363da2bb
ally-sorloth <allysorloth@gmail.com>
2020-12-17 22:06:53 +0300

    create project folder and files

---
+++ pages/users/[id].js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useRouter} from "netx/router";
+
+const User = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    return <div>User : {id}</div>;
+};
+
+export default User;
+
+}
